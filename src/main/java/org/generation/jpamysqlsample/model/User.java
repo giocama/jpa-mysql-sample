995ed3fc12a463325579eba51032886d27f41b4b
Giovanni Calleja <giocallejam@gmail.com>
2021-11-04 17:36:57 -0600

    Initial commit

---
+++ src/main/java/org/generation/jpamysqlsample/model/User.java
@@ -0,0 +1,62 @@
+package org.generation.jpamysqlsample.model;
+
+import javax.persistence.*;
+
+
+@Entity(name = "Users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column()
+    private String email;
+
+    public User()
+    {
+    }
+
+    public User( String name, Integer id )
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
